f6c5b0c3c8f207163f235dc0af3728a658ca9c0d
hhh <957400829@qq.com>
2018-04-02 21:44:23 +0800

    new io

--- src/main/java/com/iot/Channel.java
+++ src/main/java/com/iot/Channel.java
@@ -21,21 +21,11 @@ public class Channel {
 
 	public static void main(String[] args) throws IOException {
 
-
 		/**
 		 * FileChannel的transferFrom()方法可以将数据从源通道传输到FileChannel中
-		 */
-//		RandomAccessFile fromFile = new RandomAccessFile("fromFile.txt", "rw");
-//		FileChannel  fromChannel = fromFile.getChannel();
-//		RandomAccessFile toFile = new RandomAccessFile("toFile.txt", "rw");
-//		FileChannel toChannel = toFile.getChannel();
-//		long position = 0;
-//		long count = fromChannel.size();
-//		toChannel.transferFrom(position, count, fromChannel)
-
-
-		/**
-		 * 确实算一种新IO模式，以前的文件操作代码很复杂，简化代码，有一点java基础的理解起来很轻松
+		 *
+		 * 在SoketChannel的实现中，SocketChannel只会传输此刻准备好的数据（可能不足count字节）。
+		 *		因此，SocketChannel可能不会将请求的所有数据(count个字节)全部传输到FileChannel中。
 		 */
 		RandomAccessFile fromFile = new RandomAccessFile("fromFile.txt", "rw");
 		FileChannel fromChannel = fromFile.getChannel();
@@ -43,6 +33,20 @@ public class Channel {
 		FileChannel toChannel = toFile.getChannel();
 		long position = 0;
 		long count = fromChannel.size();
-		fromChannel.transferTo(position, count, toChannel);
+		toChannel.transferFrom(fromChannel,position, count);
+
+		/**
+		 * 确实算一种新IO模式，以前的文件操作代码很复杂，简化代码，有一点java基础的理解起来很轻松
+		 *
+		 * 在SoketChannel的实现中，SocketChannel只会传输此刻准备好的数据（可能不足count字节）。
+		 * 		因此，SocketChannel可能不会将请求的所有数据(count个字节)全部传输到FileChannel中。
+		 */
+//		RandomAccessFile fromFile = new RandomAccessFile("fromFile.txt", "rw");
+//		FileChannel fromChannel = fromFile.getChannel();
+//		RandomAccessFile toFile = new RandomAccessFile("toFile.txt", "rw");
+//		FileChannel toChannel = toFile.getChannel();
+//		long position = 0;
+//		long count = fromChannel.size();
+//		fromChannel.transferTo(position, count, toChannel);
 	}
 }
---
+++ src/main/java/com/iot/Selector.java
@@ -0,0 +1,57 @@
+package com.iot;
+
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by xiongxiaoyu
+ * Data:2018/4/2
+ * Time:15:50
+ *
+ *  可以只是用一个线程处理所有通道，一个线程对应一个Selector，处理多个channel
+ *
+ * 与Selector一起使用时，Channel必须处于非阻塞模式下。
+ * 这意味着不能将FileChannel与Selector一起使用，因为FileChannel不能切换到非阻塞模式。而套接字通道都可以。
+ *
+ * SelectionKey interest集合 ready集合 Channel  Selector  附加对象
+ * 访问这几个参数的方法：集合使用“位或”，其余直接获取
+ */
+public class Selector {
+
+	public static void main(String[] args) throws IOException {
+
+
+		//Selector.open()注册一个Selector
+		java.nio.channels.Selector selector= java.nio.channels.Selector.open();
+
+		//使用Selector时，Channel必须处于非阻塞模式，FileChannel不能切换非阻塞模式，套接字通道可以。
+		//将channel注册到selector上，通过SelectableChannel.register()实现，第二个参数是“interest集合”
+		//interest集合: SelectionKey.OP_READ,OP_ACCEPT,OP_CONNECT,OP_WRITE，可以使用“位或”操作符
+		channel.configureBlocking(false);
+
+		SelectionKey key = channel.register(selector, SelectionKey.OP_READ);
+
+		while(true) {
+			int readyChannels = selector.select();
+			if(readyChannels == 0) continue;
+			Set selectedKeys = selector.selectedKeys();
+			Iterator keyIterator = selectedKeys.iterator();
+			while(keyIterator.hasNext()) {
+				SelectionKey key = keyIterator.next();
+				if(key.isAcceptable()) {
+					// a connection was accepted by a ServerSocketChannel.
+				} else if (key.isConnectable()) {
+					// a connection was established with a remote server.
+				} else if (key.isReadable()) {
+					// a channel is ready for reading
+				} else if (key.isWritable()) {
+					// a channel is ready for writing
+				}
+				keyIterator.remove();
+			}
+		}
+
+	}
+}
